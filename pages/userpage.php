cc6b6afcd443daa984c53b549b13c4549f2fe1e7
noelstepha <noelstepha@eisti.eu>
2014-03-07 10:59:14 +0000

    add style.php

--- pages/userpage.php
+++ pages/userpage.php
@@ -21,7 +21,7 @@
 	<title>Network Application</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" type="text/css" href="../css/style.css"/>	
+	<link rel="stylesheet" type="text/css" href="../css/style.php"/>	
 	<link rel="stylesheet" type="text/css" href="../css/slide.css"/>	
 	<link rel="stylesheet" type="text/css" href="../css/settings.css"/>
 	<link rel="stylesheet" type="text/css" href="../css/placement.css"/>
